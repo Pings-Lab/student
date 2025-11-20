4a006110683eba72a24679be71541c7c7a934032
Pradeepkh312 <pradeepkh312@gmail.com>
2025-11-20 16:33:55 +0530

    models file created

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from extension import db
+from sqlalchemy.dialects.postgresql import JSONB
+from datetime import datetime
+
+class Auth(db.Model):
+  __tablename__='auth'
+  id=db.Column(db.String(15), primary_key=True)
+  f_name=db.Column(db.String(10))
+  l_name=db.Column(db.String(10))
+  mobile=db.Column(db.String(10))
+  email=db.Column(db.String(60))
+  password=db.Column(db.String(200))
+  created=db.Column(db.Timezone)
+  def __repr__(self):
+        return f'<Auth {self.id}>'
