8927fda9141f09a8eda9bc222dd5ee4d46938d87
Pradeepkh312 <pradeepkh312@gmail.com>
2025-11-20 14:44:49 +0530

    app.py update

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,33 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
+from config import Config
+from extension import db, jwt
 from routes.auth import auth_bp
 from routes.profile import profile_bp
 
-load_dotenv()
-app=Flask(__name__)
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(profile_bp)
+load_dotenv()
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    CORS(app, supports_credentials=True, origins=["http://127.0.0.1:5000"])
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(profile_bp)
+
+    return app
 
 if __name__=="__main__":
    host=os.getenv("HOST")
    port=os.getenv("PORT")
    debug=True if os.getenv("DEBUG")=="1" else False
+   app = create_app()
    app.run(debug=debug, host=host, port=port)
