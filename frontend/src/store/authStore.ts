8ec83b25774929558a09467e6db438dead7e5df5
Pradeepkh312 <pradeepkh312@gmail.com>
2025-11-28 18:24:20 +0530

    trying zustand

---
+++ frontend/src/store/authStore.ts
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import apiStack from "../api/apiStack";
+
+interface AuthState {
+  email: string;
+  password: string;
+  message: string;
+  loading: boolean;
+  isAuthenticated: boolean;
+
+  setEmail: (v: string) => void;
+  setPassword: (v: string) => void;
+  loginUser: () => Promise<void>;
+  logout: () => void;
+}
+
+export const useAuth = create<AuthState>()(
+  persist(
+    (set, get) => ({
+      email: "",
+      password: "",
+      message: "",
+      loading: false,
+      isAuthenticated: false,
+
+      setEmail: (v) => set({ email: v }),
+      setPassword: (v) => set({ password: v }),
+
+      loginUser: async () => {
+        const { email, password } = get();
+        try {
+          set({ loading: true });
+          const res = await apiStack.login({ email, password });
+
+          set({
+            message: res.data.message,
+            loading: false,
+            isAuthenticated: true,
+            email: "",
+            password: ""
+          });
+        } catch (err: any) {
+          set({
+            loading: false,
+            message: err.response?.data?.message || "Login failed",
+            isAuthenticated: false
+          });
+        }
+      },
+
+      logout: () =>
+        set({
+          isAuthenticated: false,
+          email: "",
+          password: "",
+          message: ""
+        }),
+    }),
+
+    {
+      name: "auth-store", // localStorage key
+      partialize: (state) => ({
+        email: state.email,
+        password: state.password,
+      }),
+    }
+  )
+);
